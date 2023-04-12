f8f14dfe8353ff0fa2a35e5f6cc600cefbf5ec9b
Nadzeya <lustra2013@gmail.com>
2023-04-12 09:58:58 +0300

    add logger

---
+++ logg.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename="my_log.log",
+    format="%(asctime)s - %(module)s - %(levelname)s - %(funcName)s - %(message)s",
+    datefmt='%d-%m-%Y %H:%M:%S',
+    encoding='utf-8',
+)
